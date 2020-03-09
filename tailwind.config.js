3e114904f3343b709b7580fd97203fa57fcb3e43
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-03-09 15:16:30 +0100

    Followed code styling for tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 const blueColor = {
   default: 'hsl(222,13%,15%)',
-  500: 'hsl(222,13%,15%)'
+  500: 'hsl(222,13%,15%)',
 };
 
 const taupeColor = {
   default: 'hsl(37,48%,87%)',
-  500: 'hsl(37,48%,87%)'
-}
+  500: 'hsl(37,48%,87%)',
+};
 
 module.exports = {
   theme: {
     extend: {},
     colors: {
-      primary: {...blueColor},
-      secondary: {...taupeColor},
-      blue: {...blueColor},
-      taupe: {...taupeColor}
+      primary: { ...blueColor },
+      secondary: { ...taupeColor },
+      blue: { ...blueColor },
+      taupe: { ...taupeColor },
     },
   },
   variants: {},
   plugins: [],
-}
+};
