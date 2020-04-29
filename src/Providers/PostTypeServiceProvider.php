1a3ae9f5aa09b3d81d053cb39d4fbcce483681f0
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-04-29 12:58:32 +0200

    Added new fields to a lot.

--- src/Providers/PostTypeServiceProvider.php
+++ src/Providers/PostTypeServiceProvider.php
@@ -49,13 +49,17 @@ class PostTypeServiceProvider implements ServiceProvider
 			\Carbon_Fields\Carbon_Fields::boot();
 		}
 
-		Container::make('post_meta', __('Lot info', 'werf8'))
+		Container::make('post_meta', __('Woning info', 'werf8'))
 			->where('post_type', '=', 'lot')
 			->add_fields([
-				Field::make('text', 'price', __('Price', 'werf8'))
+				Field::make('text', 'price', __('Prijs', 'werf8'))
 					->set_attribute('placeholder' , 'xxx.xxx,-'),
-				Field::make('text', 'surface_area', __('Surface Area', 'werf8'))
+				Field::make('text', 'surface_area', __('Oppervlakte', 'werf8'))
 					->set_attribute('placeholder', 'xx'),
+				Field::make('text', 'outside_area', __('Buitenruimte', 'werf8'))
+					->set_attribute('placeholder', 'xx'),
+				Field::make('text', 'location', __('Ligging', 'werf8'))
+					->set_attribute('placeholder', '1e verdieping'),
 				Field::make('select', 'status', __('Status', 'werf8'))
 					->set_options([
 						'soon' => 'Binnenkort in de verkoop',
