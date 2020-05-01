0f886ba44fe714149a3a8fd9b05ef0b4d6df05ad
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-05-01 10:07:49 +0200

    password page fix

--- index.php
+++ index.php
@@ -6,14 +6,15 @@ use Timber\Timber;
 $context = Timber::get_context();
 
 $context['post'] = new Post();
+$templates = [
+	'views/page.html.twig',
+];
 
 if (post_password_required($context['post']->id)) {
 	array_unshift($templates, 'views/single/password.html.twig');
 }
 
 return Timber::render(
-    [
-        'views/page.html.twig',
-    ],
+    $templates,
     $context
 );
